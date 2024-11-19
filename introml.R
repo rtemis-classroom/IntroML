dd081443241c586ad58da50a5a37161aab6a592d
Stathis Gennatas <gennatas@gmail.com>
2024-11-19 10:44:30 -0800

    up

--- introml.R
+++ introml.R
@@ -1,6 +1,6 @@
 # IntroML Lab
 # 2024-11-19
-# EDG rtemis.org
+# EDG rtemis.org | Stathis.Gennatas@ucsf.edu
 
 # Packages ----
 install.packages("rtemis", repos = c("https://egenn.r-universe.dev", "https://cloud.r-project.org"))
@@ -8,13 +8,15 @@ install.packages(c(
   "data.table", "R6", "plyr", "progressr", "future.apply",
   "glmnet", "rpart", "ranger", "lightgbm", "plotly"
 ))
+# If more dependencies are needed, the functions bellow will let you know
 
 # Load package
 library(rtemis)
 
 # Data ----
 # Source: https://www.openml.org/search?type=data&sort=runs&status=active&qualities.NumberOfInstances=between_1000_10000&qualities.NumberOfFeatures=gte_0&qualities.NumberOfClasses=lte_1&tags.tag=medicine&id=43672
-# Read data
+# Download and unzip
+# Read data (change this to your local path)
 dat <- read("~/icloud/Data/OpenML/43672_HeartDisease/dataset.arff")
 
 # Preprocess ----
@@ -27,8 +29,8 @@ nunique_perfeat(dat)
 dat <- preprocess(dat, len2factor = 4)
 
 # Model ----
-
 ## LASSO ----
+# This will automatically tune the regularization parameter lambda.
 dat_lasso <- train_cv(
   dat,
   alg = "glmnet",
@@ -47,17 +49,17 @@ dat_rf <- train_cv(
   alg = "ranger"
 )
 
-dat_lightrf <- train_cv(
-  dat,
-  alg = "lightrf"
-)
-
 ## LightGBM ----
+# This automatically tunes the number of trees, which is very important.
+# There are many more hyperparameters you can tune.
 dat_lightgbm <- train_cv(
   dat,
   alg = "lightgbm"
 )
 
 ## Present results ----
+# Plot training and testing performance of all models
 present(dat_lasso, dat_cart, dat_rf, dat_lightgbm)
+
+# Describe the gradient boosting model
 describe(dat_lightgbm)
